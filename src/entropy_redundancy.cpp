55cdf80f3577148fa0f0897f1f566fa5d3faf209
hoangthetruong <thetruongh36@gmail.com>
2026-04-10 17:20:57 +0700

    Update entropy_redundancy.cpp

--- src/entropy_redundancy.cpp
+++ src/entropy_redundancy.cpp
@@ -4,7 +4,7 @@
 #include <set>
 using namespace std;
 
-// Hàm tính entropy
+// Tính entropy
 double calculate_entropy(const string &input) {
     map<char, int> freq;
 
@@ -23,7 +23,7 @@ double calculate_entropy(const string &input) {
     return entropy;
 }
 
-// 👉 BẠN PHẢI VIẾT HÀM NÀY
+// 👉 BỔ SUNG HÀM NÀY
 double calculate_redundancy(const string &input) {
     double H = calculate_entropy(input);
 
@@ -35,25 +35,4 @@ double calculate_redundancy(const string &input) {
     double H_max = log2(alphabet_size);
 
     return 1.0 - (H / H_max);
-}
-
-// test nhanh
-int main() {
-    string s1 = "aaaa";
-    string s2 = "abcd";
-    string s3 = "aabbcc";
-
-    cout << "Input: " << s1 << endl;
-    cout << "Entropy: " << calculate_entropy(s1) << endl;
-    cout << "Redundancy: " << calculate_redundancy(s1) << endl;
-
-    cout << "\nInput: " << s2 << endl;
-    cout << "Entropy: " << calculate_entropy(s2) << endl;
-    cout << "Redundancy: " << calculate_redundancy(s2) << endl;
-
-    cout << "\nInput: " << s3 << endl;
-    cout << "Entropy: " << calculate_entropy(s3) << endl;
-    cout << "Redundancy: " << calculate_redundancy(s3) << endl;
-
-    return 0;
 }
